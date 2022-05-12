46eedeede2c02dd5d6c222726e6a15ef4132c43b
vicwski <90584286+vicwski@users.noreply.github.com>
2022-05-12 10:23:15 -0300

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,77 @@
+class Game {
+  constructor() {}
+
+  getState() {
+    var gameStateRef = database.ref('gameState')
+    gameStateRef.on('value', function (data) {
+      gameState = data.val()
+    })
+  }
+
+  update(state) {
+    database.ref('/').update({
+      gameState: state
+    })
+  }
+
+  start() {
+    player = new Player()
+    playerCount = player.getCount()
+
+    form = new Form()
+    form.display()
+
+    car1 = createSprite(width / 2 - 50, height - 100)
+    car1.addImage('car1', car1_img)
+    car1.scale = 0.07
+
+    car2 = createSprite(width / 2 + 100, height - 100)
+    car2.addImage('car2', car2_img)
+    car2.scale = 0.07
+
+    //Matriz que guarda os carros
+    //index  0  ,  1
+    cars = [car1, car2]
+  }
+
+  handleElements() {
+    form.hide()
+    form.titleImg.position(40, 50)
+    form.titleImg.class('gameTitleAfterEffect')
+  }
+
+  play() {
+    this.handleElements()
+
+    Player.getPlayersInfo()
+
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6)
+
+      //índice da matriz
+      var index = 0
+      for (var plr in allPlayers) {
+        //adicione 1 ao índice para cada loop
+        index += 1
+        // use os dados do banco de dados para exibir os carros nas direções x e y
+        var x = allPlayers[plr].positionX
+        var y = height - allPlayers[plr].positionY
+
+        cars[index - 1].position.x = x
+        cars[index - 1].position.y = y
+      }
+
+      //   this.handlePlayerControls()
+
+      drawSprites()
+    }
+  }
+
+  handlePlayerControls() {
+    // manipulando eventos de teclado
+    if (keyIsDown(UP_ARROW)) {
+          player.positionY += 10
+          player.update()
+    }
+  }
+}
